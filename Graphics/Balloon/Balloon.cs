184e79cde2d71c83b61e730fb916d91923616e1e
CPU <claudio.pucci@gmail.com>
2020-09-10 17:30:26 +0200

    Back walk, improved balloon
    Added also basic test scenes for me and Gorbit

--- Graphics/Balloon/Balloon.cs
+++ Graphics/Balloon/Balloon.cs
@@ -6,28 +6,54 @@ public class Balloon : MonoBehaviour {
   public TMPro.TextMeshPro text;
   public SpriteRenderer body;
   public SpriteRenderer tip;
+  Transform anchor;
+  Vector2 size = Vector2.zero;
+  int numWords = 0;
+  float delay = 0;
 
   private void Awake() {
     b = this;
+    gameObject.SetActive(false);
   }
 
   public static void Show(string message, Transform speaker) {
-    Vector2 size = b.text.GetPreferredValues(message);
+    b.anchor = speaker;
+    b.delay = 1000;
+    b.gameObject.SetActive(true);
+    b.size = b.text.GetPreferredValues(message);
     b.text.text = message;
 
     Vector3 location = speaker.position;
-    location.y += 8f + size.y / 2;
+    location.y += 8f + b.size.y / 2;
     location.x -= 2f;
     location.z = -.5f;
     b.transform.position = location;
 
-    size.x += .5f;
-    size.y += .5f;
-    b.body.size = size;
+    b.size.x += .5f;
+    b.size.y += .5f;
+    b.body.size = b.size;
 
-    b.tip.transform.localPosition = new Vector3(-size.x / 4, -size.y / 2 + 0.235f, 0);
+    b.tip.transform.localPosition = new Vector3(-b.size.x / 4, -b.size.y / 2 + 0.235f, 0);
+
+    b.numWords = 0;
+    foreach (char c in message)
+      if (char.IsWhiteSpace(c)) b.numWords++;
+
+    b.delay = 02f * b.numWords;
   }
 
+  private void Update() {
+    if (b.delay <= 0) return;
+    b.delay -= Time.deltaTime;
+    if (b.delay <= 0) gameObject.SetActive(false);
+
+    Vector3 location = b.anchor.position;
+    location.y += 8f + size.y / 2;
+    location.x -= 2f;
+    location.z = -.5f;
+    b.transform.position = location;
+    b.tip.transform.localPosition = new Vector3(-b.size.x / 4, -b.size.y / 2 + 0.235f, 0);
+  }
 
 }
 
