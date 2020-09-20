bb293cd0773942c3fe2254ee269b741f92b93e62
CPU <claudio.pucci@gmail.com>
2020-09-20 23:07:04 +0200

    Improved new paths inspector

--- Items/Path.cs
+++ Items/Path.cs
@@ -9,6 +9,10 @@ public class Path : MonoBehaviour {
   public Vector3 tr = new Vector3(1, 1);
   public Vector3 bl = new Vector3(0, 0);
   public Vector3 br = new Vector3(1, 0);
+  public Path left;
+  public Path right;
+  public Path up;
+  public Path down;
 
   Mesh mesh;
   MainPath parent;
@@ -50,6 +54,8 @@ public class Path : MonoBehaviour {
 
 [CustomEditor(typeof(Path))]
 public class PathEditor : Editor {
+  MainPath parent;
+
   void OnSceneGUI() {
     Path t = target as Path;
     if (!t.ShowPaths) return;
@@ -63,25 +69,92 @@ public class PathEditor : Editor {
     if (EditorGUI.EndChangeCheck()) {
       Undo.RecordObject(t, "TL");
       t.tl = np;
+      // Try to snap
+      if (parent == null) {
+        Transform p = t.transform.parent;
+        if (p != null) {
+          parent = p.GetComponent<MainPath>();
+        }
+      }
+      if (parent == null) return;
+      foreach (Transform sib in parent.transform) {
+        if (sib == t.transform) continue;
+        Path p = sib.GetComponent<Path>();
+        Vector3 edge = p.tr;
+        if (np != edge && Vector3.Distance(np, edge) < .05f) {
+          Debug.Log("Snap!");
+          t.tl = edge;
+        }
+      }
     }
     np = Handles.FreeMoveHandle(t.tr, Quaternion.identity, size, snap, Handles.RectangleHandleCap);
     if (EditorGUI.EndChangeCheck()) {
       Undo.RecordObject(t, "TR");
       t.tr = np;
+      // Try to snap
+      if (parent == null) {
+        Transform p = t.transform.parent;
+        if (p != null) {
+          parent = p.GetComponent<MainPath>();
+        }
+      }
+      if (parent == null) return;
+      foreach (Transform sib in parent.transform) {
+        if (sib == t.transform) continue;
+        Path p = sib.GetComponent<Path>();
+        Vector3 edge = p.tl;
+        if (np != edge && Vector3.Distance(np, edge) < .05f) {
+          Debug.Log("Snap!");
+          t.tr = edge;
+        }
+      }
     }
     np = Handles.FreeMoveHandle(t.bl, Quaternion.identity, size, snap, Handles.RectangleHandleCap);
     if (EditorGUI.EndChangeCheck()) {
       Undo.RecordObject(t, "BL");
       t.bl = np;
+      // Try to snap
+      if (parent == null) {
+        Transform p = t.transform.parent;
+        if (p != null) {
+          parent = p.GetComponent<MainPath>();
+        }
+      }
+      if (parent == null) return;
+      foreach (Transform sib in parent.transform) {
+        if (sib == t.transform) continue;
+        Vector3 edge = sib.GetComponent<Path>().br;
+        if (np != edge && Vector3.Distance(np, edge) < .05f) {
+          Debug.Log("Snap!");
+          t.bl = edge;
+        }
+      }
     }
     np = Handles.FreeMoveHandle(t.br, Quaternion.identity, size, snap, Handles.RectangleHandleCap);
     if (EditorGUI.EndChangeCheck()) {
       Undo.RecordObject(t, "BR");
       t.br = np;
+      // Try to snap
+      if (parent == null) {
+        Transform p = t.transform.parent;
+        if (p != null) {
+          parent = p.GetComponent<MainPath>();
+        }
+      }
+      if (parent == null) return;
+      foreach (Transform sib in parent.transform) {
+        if (sib == t.transform) continue;
+        Vector3 edge = sib.GetComponent<Path>().bl;
+        if (np != edge && Vector3.Distance(np, edge) < .05f) {
+          Debug.Log("Snap!");
+          t.br = edge;
+        }
+      }
     }
   }
 
   public override void OnInspectorGUI() {
+    DrawDefaultInspector();
     if (GUILayout.Button("Set collider")) {
       Path t = target as Path;
       PolygonCollider2D poly = t.GetComponent<PolygonCollider2D>();
@@ -91,6 +164,46 @@ public class PathEditor : Editor {
       }
 
       poly.points = new Vector2[] { t.tl, t.tr, t.br, t.bl };
+    }
+
+    if (GUILayout.Button("Set siblings")) {
+      Path t = target as Path;
+      t.left = null;
+      t.right = null;
+      t.up = null;
+      t.down = null;
+
+      if (parent == null) {
+        Transform p = t.transform.parent;
+        if (p != null) {
+          parent = p.GetComponent<MainPath>();
+        }
+      }
+      if (parent == null) {
+        Debug.LogError("Missing main parent!");
+        return;
+      }
+
+      // Find the siblings. They need to have both edges snapped
+      foreach (Transform sib in parent.transform) {
+        if (sib == t.transform) continue;
+        Path p = sib.GetComponent<Path>();
+        
+        if (t.tl == p.tr && 
+            t.bl == p.br) 
+          t.left = p;
+        if (t.tr == p.tl && 
+            t.br == p.bl) 
+          t.right = p;
+
+        if (t.tr == p.br && 
+            t.tl == p.bl) 
+          t.up = p;
+        if (t.br == p.tr && 
+            t.bl == p.tl) 
+          t.down = p;
+      }
+
     }
   }
 }
