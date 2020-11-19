ab3ed7e62ec65d68f3bb7fe95346423b8f665f45
CPU <claudio.pucci@gmail.com>
2020-11-19 12:36:30 +0100

    Edwige
    And fixel the "Actor scale" parameter that was ignored

--- Actors/Actor.cs
+++ Actors/Actor.cs
@@ -498,11 +498,17 @@ public class Actor : MonoBehaviour {
     if (ty > currentRoom.maxY) ty = currentRoom.maxY;
     float scaley = -.05f * (ty - currentRoom.minY - 1.9f) + .39f;
 
-    scaley *= currentRoom.scalePerc;
+    scaley *= currentRoom.scalePerc * mainScale;
     transform.localScale = new Vector3(scaley, scaley, 1);
     int zpos = (int)(scaley * 10000);
-    Face.sortingOrder = zpos + 1;
-    Arms.sortingOrder = zpos + 2;
+    if (!male && dir != Dir.F) {
+      Face.sortingOrder = zpos + 2;
+      Arms.sortingOrder = zpos + 1;
+    }
+    else {
+      Face.sortingOrder = zpos + 1;
+      Arms.sortingOrder = zpos + 2;
+    }
     Legs.sortingOrder = zpos;
     if (cape) cape.SetSortingOrder(zpos);
   }
